215026f4637bc3b2a97106eb234ba6af1fc91fe7
Christabel Johnson <christabel.a.johnson@accenture.com>
2026-07-20 12:18:36 +0100

    delete and create category service methods complete, others have flow outlined

--- category/src/main/java/com/christabelj/expenses/category/service/CategoryServiceImpl.java
+++ category/src/main/java/com/christabelj/expenses/category/service/CategoryServiceImpl.java
@@ -3,7 +3,9 @@ package com.christabelj.expenses.category.service;
 import com.christabelj.expenses.category.domain.dto.request.CreateCategoryRequestDto;
 import com.christabelj.expenses.category.domain.dto.request.UpdateCategoryRequestDto;
 import com.christabelj.expenses.category.domain.dto.response.CategoryResponseDto;
+import com.christabelj.expenses.category.domain.entity.Category;
 import com.christabelj.expenses.category.mapper.CategoryMapper;
+import com.christabelj.expenses.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,24 +16,30 @@ import java.util.UUID;
 
 public class CategoryServiceImpl implements CategoryService {
     private CategoryMapper categoryMapper;
+    private CategoryRepository categoryRepository;
 
     @Override
     public CategoryResponseDto createCategory(CreateCategoryRequestDto requestDto) {
-        return null;
+        // method flow: request DTO → map to entity → database operation → map to response DTO → return
+        Category category = categoryMapper.toCategoryEntity(requestDto);
+        Category savedEntity = categoryRepository.save(category);
+        return categoryMapper.toResponseDto(savedEntity);
     }
 
     @Override
     public List<CategoryResponseDto> getAllCategories() {
+        // flow: Get all entities from db -> convert list to stream -> map each to response dto -> convert all back to a list
         return List.of();
     }
 
     @Override
     public CategoryResponseDto updateCategory(UUID id, UpdateCategoryRequestDto requestDto) {
+        // flow: use ID to find existing entity -> change attributes to the ones in the request -> save new entity -> map to response dto
         return null;
     }
 
     @Override
     public void deleteCategory(UUID id) {
-
+        categoryRepository.deleteById(id);
     }
 }
