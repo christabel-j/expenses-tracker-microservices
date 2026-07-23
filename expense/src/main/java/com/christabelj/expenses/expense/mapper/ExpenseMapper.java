bca86263f3eea47403b24f92948aab7e7a8cd6b5
Christabel Johnson <christabel.a.johnson@accenture.com>
2026-07-23 13:15:16 +0100

    mapper interface and implementation completed

--- expense/src/main/java/com/christabelj/expenses/expense/mapper/ExpenseMapper.java
+++ expense/src/main/java/com/christabelj/expenses/expense/mapper/ExpenseMapper.java
@@ -1,4 +1,17 @@
 package com.christabelj.expenses.expense.mapper;
 
+import com.christabelj.expenses.expense.domain.dto.request.CreateExpenseRequestDto;
+import com.christabelj.expenses.expense.domain.dto.request.UpdateExpenseRequestDto;
+import com.christabelj.expenses.expense.domain.dto.response.ExpenseResponseDto;
+import com.christabelj.expenses.expense.domain.entity.Expense;
+
 public interface ExpenseMapper {
+
+    // Request DTO to Entity
+    Expense toEntity(CreateExpenseRequestDto requestDto);
+
+    Expense toEntity(UpdateExpenseRequestDto requestDto);
+
+    // Entity to Response DTO
+    ExpenseResponseDto toResponseDto(Expense expenseEntity);
 }
--- expense/src/main/java/com/christabelj/expenses/expense/mapper/ExpenseMapperImpl.java
+++ expense/src/main/java/com/christabelj/expenses/expense/mapper/ExpenseMapperImpl.java
@@ -1,4 +1,39 @@
 package com.christabelj.expenses.expense.mapper;
 
+import com.christabelj.expenses.expense.domain.dto.request.CreateExpenseRequestDto;
+import com.christabelj.expenses.expense.domain.dto.request.UpdateExpenseRequestDto;
+import com.christabelj.expenses.expense.domain.dto.response.ExpenseResponseDto;
+import com.christabelj.expenses.expense.domain.entity.Expense;
+
 public class ExpenseMapperImpl implements ExpenseMapper {
+    @Override
+    public Expense toEntity(CreateExpenseRequestDto requestDto) {
+        return Expense.builder()
+                .amount(requestDto.amount())
+                .description(requestDto.description())
+                .categoryId(requestDto.categoryId())
+                .date(requestDto.date())
+                .build();
+    }
+
+    @Override
+    public Expense toEntity(UpdateExpenseRequestDto requestDto) {
+         return Expense.builder()
+                .amount(requestDto.amount())
+                .description(requestDto.description())
+                .categoryId(requestDto.categoryId())
+                .date(requestDto.date())
+                .build();
+    }
+
+    @Override
+    public ExpenseResponseDto toResponseDto(Expense expenseEntity) {
+        return ExpenseResponseDto.builder()
+                .id(expenseEntity.getId())
+                .amount(expenseEntity.getAmount())
+                .description(expenseEntity.getDescription())
+                .categoryId(expenseEntity.getCategoryId())
+                .date(expenseEntity.getDate())
+                .build();
+    }
 }
