77b20dd365bc22ec0112a42a956e7d621c42b1fd
Christabel Johnson <christabel.a.johnson@accenture.com>
2026-07-23 09:37:14 +0100

    expense Entity layer

--- expense/src/main/java/com/christabelj/expenses/expense/domain/entity/Expense.java
+++ expense/src/main/java/com/christabelj/expenses/expense/domain/entity/Expense.java
@@ -1,8 +1,11 @@
 package com.christabelj.expenses.expense.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
 @Table(name = "expenses")
@@ -16,4 +19,20 @@ import lombok.*;
 @Builder
 
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "amount", nullable = false, precision = 10, scale = 2) // precision = 10 — total number of digits (10 digits total), scale = 2 — digits after the decimal point
+    private BigDecimal amount;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "categoryId", nullable = false, updatable = false)
+    private UUID categoryId;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
 }
