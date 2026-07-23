4e5506f705d23711323f5fa7654ccad3b226e15a
Christabel Johnson <christabel.a.johnson@accenture.com>
2026-07-23 10:32:48 +0100

    other dtos complete

--- expense/src/main/java/com/christabelj/expenses/expense/domain/dto/request/UpdateExpenseRequestDto.java
+++ expense/src/main/java/com/christabelj/expenses/expense/domain/dto/request/UpdateExpenseRequestDto.java
@@ -1,4 +1,26 @@
 package com.christabelj.expenses.expense.domain.dto.request;
 
-public record UpdateExpenseRequestDto() {
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record UpdateExpenseRequestDto(
+        @NotNull
+        @Positive
+        @Digits(integer = 10, fraction = 2)
+        BigDecimal amount,
+
+        @NotBlank
+        @Size(max = 255)
+        String description,
+
+        @NotNull
+        UUID categoryId,
+
+        @NotNull
+        @PastOrPresent
+        LocalDate date
+) {
 }
--- expense/src/main/java/com/christabelj/expenses/expense/domain/dto/response/ExpenseResponseDto.java
+++ expense/src/main/java/com/christabelj/expenses/expense/domain/dto/response/ExpenseResponseDto.java
@@ -1,4 +1,17 @@
 package com.christabelj.expenses.expense.domain.dto.response;
 
-public record ExpenseResponseDto() {
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Builder
+public record ExpenseResponseDto(
+        UUID id,
+        BigDecimal amount,
+        String description,
+        UUID categoryId, // soon maybe category name?
+        LocalDate date
+) {
 }
